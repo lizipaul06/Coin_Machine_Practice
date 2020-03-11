57914d7cdb89bb89d18b0d6a0e23fa8b6fbde20f
Elizabeth Paul <elizabethpaul@Elizabeths-MacBook-Pro.local>
2020-03-11 15:57:07 +0000

    coins

--- test.js
+++ test.js
@@ -1,6 +1,19 @@
 var VendingMachine = function () {
     this.balance = 0;
+    this.coins = {
+      "nickel": 0.5,
+      "dime": 0.1,
+      "quarter": 0.25,
+      "dollar": 1
+    };
 };
+
+
+
+VendingMachine.prototype.insertMoney = function(coin){
+  this.balance += this.coins[coin];
+
+}
 
 VendingMachine.prototype.getBalance = function () {
     return this.balance;
@@ -15,5 +28,11 @@ describe('Vending Machine', function () {
             machine = new VendingMachine();
             assert.strictEqual(0, machine.getBalance())
         });
+        it('Tracks the value of a single coin inserted', function(){
+          machine.insertMoney("nickel")
+            assert.strictEqual(0.5, machine.getBalance())
+
+        });
     });
+
 });
